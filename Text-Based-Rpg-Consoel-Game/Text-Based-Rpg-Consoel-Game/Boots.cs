a88971e7e2225a5dad84ac14ef676123b2d65b38
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-06-30 05:08:29 +0300

    Boots class added

---
+++ Text-Based-Rpg-Consoel-Game/Text-Based-Rpg-Consoel-Game/Boots.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Text_Based_Rpg_Consoel_Game.Items
+{
+    class Boots : Item
+    {
+        private float speed;
+
+        public float Speed
+        {
+            get { return speed * Level; }
+            set
+            {
+                if (speed <= 0)
+                {
+                    speed = 0;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+        public Boots(string itemName, float itemBuyPrice, int itemSolidity, float speed)
+            : base(itemName, itemBuyPrice, itemSolidity)
+        {
+            this.speed = speed;
+        }
+    }
+}
